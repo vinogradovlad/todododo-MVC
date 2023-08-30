f3494ea7f97c4e8ba843966570a046a99079fdb2
vinogradovlad <143588592+vinogradovlad@users.noreply.github.com>
2023-08-30 17:25:03 +0700

    Add files via upload

---
+++ js/model.js
@@ -0,0 +1,65 @@
+export default class Model {
+    constructor (){
+
+        this.tasks = [];
+        this.loadFromLocalStorage();
+    }
+
+    addTask(text) {
+        let id = 1;
+        if (this.tasks.length > 0) {
+        id = this.tasks[this.tasks.length - 1]['id'] + 1;
+        };
+
+        const newTask = {
+            id: id,
+            status:'active',
+            text: text,
+        };
+
+        this.tasks.push(newTask);
+        this.saveToLocalStorage();
+
+        return newTask;
+    }
+
+    changeStatus(task){
+        if (task.status === 'active') {
+            task.status = 'done'
+        } else {
+            task.status = 'active';
+        }
+
+        this.saveToLocalStorage();
+    }
+
+    removeTask(task){
+       const index = this.tasks.indexOf(task);
+       this.tasks.splice(index, 1);
+       this.saveToLocalStorage();
+
+    }
+
+    findTask(id){
+        const task = this.tasks.find(function(task){
+            if (task.id === parseInt(id)) {
+                return true;
+            }
+        })
+        return task;
+    }
+
+
+    saveToLocalStorage(){
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+
+    loadFromLocalStorage(){
+        const data = localStorage.getItem('tasks');
+        if (data) {
+            this.tasks = JSON.parse(data);
+
+        }
+
+    }
+}
---
+++ js/view.js
@@ -0,0 +1,64 @@
+export default class View {
+    constructor(tasks) {
+
+        tasks.forEach((task) => {
+            this.renderTask(task); 
+        })
+        }
+
+        
+        elements = {
+            input: document.getElementById('newTask'),
+            form: document.getElementById('form'),
+            tasksList: document.getElementById('tasksList'),
+        };
+
+
+        renderTask(taskObject) {
+
+            const completeClass = taskObject.status === 'done' ? 'completed' : '';
+            const checked = taskObject.status === 'done' ? 'checked' : '';
+                
+            
+            const taskHTML = `<li class="todo-item" data-id="${taskObject.id}">
+                <label class="todo-item-label">
+                    <input class="checkbox" type="checkbox" ${checked} />
+                    <span class=${completeClass}>${taskObject.text}</span>
+                    <button class="btn btn-secondary btn-sm" data-delete>Удалить</button>
+                </label>
+            </li>`;
+
+            this.elements.tasksList.insertAdjacentHTML('beforeend', taskHTML);
+        };
+
+        
+        clearInput() {
+            this.elements.input.value = '';
+        };
+
+
+        changeStatus(taskObject){
+            const taskElement = this.elements.tasksList.querySelector(`[data-id="${taskObject.id}"]`)
+            const taskTextEl = taskElement.querySelector('span');
+                 if (taskObject.status === 'done') {
+                    taskTextEl.classList.add('completed');
+                 } else {
+                    taskTextEl.classList.remove('completed');
+                 }  
+            console.log(taskTextEl)
+
+        }
+
+
+        removeTask(taskObject){
+            const taskElement = this.elements.tasksList.querySelector(`[data-id="${taskObject.id}"]`);
+            taskElement.remove();
+
+        }
+
+    }
+
+
+ 
+
+
